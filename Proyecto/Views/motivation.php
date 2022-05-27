c134ce8fb1f5f53a63eafd4ba8caa2f972605f13
Sebas Lopez <sebastianlolpez@gmail.com>
2022-05-27 16:06:03 -0500

    Cambios

--- Proyecto/Views/motivation.php
+++ Proyecto/Views/motivation.php
@@ -10,33 +10,34 @@ define('IMG_PATH', '../img/');
 
 include(VIEWS_PATH . "header.php");
 ?>
+<body>
+	
 
-<br>
-
-
-<div class="container-fluid">
-  
+<br >
+<div class="fondo">
+	<div class="container-fluid">
 	<div class="row">
-		<div class="col-md-12">
-			<div class="card">
-				<h5 class="card-header">
-					Te Damos Razones para que juegues Futbol
-				</h5>
-				<div class="card-body">
-					<p class="card-text"  >
-						Jugar Futbol Es importante porque te ayuda a tu salud por el esfuerzo que debes de hacer, además no hay nada mejor que dejar todo por la pasión de la 
-            camiseta, imagínate con tus amigos o familia eso también es chévere y una razón mas para compartir y no te cuesta nada. 
-            <br>
-            En los torneos dar a máximo tu nivel que tienes para que puedas destacar, quien sabe que una casa talentos este buscando personas.
-          <br>
-          Por eso te invitamos a que le des clic aquí para que puedas empezar a disfrutar de las canchas disponibles
+			<div class="col-md-12">
+				<div class="jumbotron">
+					<h2>
+					Hola Amante al deporte: 
+					</h2>
+					<p style=>
+					Jugar Futbol Es importante porque te ayuda a tu salud por el esfuerzo que debes de hacer, además no hay nada mejor que dejar todo por la pasión de la 
+            		camiseta, imagínate con tus amigos o familia eso también es chévere y una razón mas para compartir y no te cuesta nada. 
+            		<br>
+            		En los torneos dar a máximo tu nivel que tienes para que puedas destacar, quien sabe que una casa talentos este buscando personas.
+          			<br>
+
+			  		 Por eso te invitamos a que le des click aquí para que puedas empezar a disfrutar de las canchas disponibles
 
 					</p>
+				
+					<a class="btn btn-primary btn-large" href="login.php">Ingresar</a>
+				
 				</div>
-				<div class="card-footer">
-					<button>Reserva</button>
-				</div>
-			</div>
 		</div>
 	</div>
+	</div>
 </div>
+</body>
